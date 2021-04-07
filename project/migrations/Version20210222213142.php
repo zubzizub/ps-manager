77213d46d3098d32287e5e5e69f9a30893dceba5
zubkov <zubzizub85@gmail.com>
2021-04-07 10:02:17 +0200

    Fixes migration

--- project/migrations/Version20210222213142.php
+++ project/migrations/Version20210222213142.php
@@ -23,9 +23,13 @@ final class Version20210222213142 extends AbstractMigration
         $this->addSql('CREATE TABLE auth_user_networks (id UUID NOT NULL, user_id UUID NOT NULL, network VARCHAR(32) DEFAULT NULL, identity VARCHAR(32) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_3EA78C3BA76ED395 ON auth_user_networks (user_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_3EA78C3B608487BC6A95E9C4 ON auth_user_networks (network, identity)');
+        $this->addSql('COMMENT ON COLUMN auth_user_networks.user_id IS \'(DC2Type:auth_user_id)\'');
         $this->addSql('CREATE TABLE auth_users (id UUID NOT NULL, email VARCHAR(255) DEFAULT NULL, status VARCHAR(16) NOT NULL, password_hash VARCHAR(500) NOT NULL, confirm_token VARCHAR(255) DEFAULT NULL, date DATE NOT NULL, role VARCHAR(255) NOT NULL, reset_token_token VARCHAR(255) DEFAULT NULL, reset_token_expires DATE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D8A1F49CE7927C74 ON auth_users (email)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D8A1F49C86EC69F0 ON auth_users (reset_token_token)');
+        $this->addSql('COMMENT ON COLUMN auth_users.id IS \'(DC2Type:auth_user_id)\'');
+        $this->addSql('COMMENT ON COLUMN auth_users.email IS \'(DC2Type:auth_user_email)\'');
+        $this->addSql('COMMENT ON COLUMN auth_users.role IS \'(DC2Type:auth_user_role)\'');
         $this->addSql('COMMENT ON COLUMN auth_users.date IS \'(DC2Type:date_immutable)\'');
         $this->addSql('COMMENT ON COLUMN auth_users.reset_token_expires IS \'(DC2Type:date_immutable)\'');
         $this->addSql('ALTER TABLE auth_user_networks ADD CONSTRAINT FK_3EA78C3BA76ED395 FOREIGN KEY (user_id) REFERENCES auth_users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
